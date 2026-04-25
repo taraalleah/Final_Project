6b0350f62ff2d063b70b5eed007d56e0a6d38f14
ADMIN <josh.dave.bengaura@gmail.com>
2026-04-25 22:26:19 +0300

    storage testing

--- JoshuaFolder/data_reading.py
+++ JoshuaFolder/data_reading.py
@@ -1,19 +1,22 @@
 import json
 
+a = open("data.json", "r")
+storage = json.load(a)
+                   #set json file as list for entry storage
+
 data = {
-    "patient" : "Dummy",
-    "hr" : 40,
-    "bp" : 120,
-    "timestamp" : 1234556636364353
-    }
+    "patient" : "Hillary Clinton",
+    "hr" : 90,              
+    "bp" : 138,
+    "timestamp" : 1234556533342424
+    }                        #dummy data
 
-a = open("data.json", "w")
 
-json.dump(data, a)
-a.close()
+storage.append(data)         #append "duh"
 
-b = open("data.json", "r")
-loaded = json.load(b)
-b.close()
+b = open("data.json", "w")
+json.dump(storage, b)
+b.close()                    #dump to json file
 
-print(loaded)
+
+print(storage)
