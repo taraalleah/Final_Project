cea53a6d00cda3c6b9c68db67a10afd701094bf8
ADMIN <josh.dave.bengaura@gmail.com>
2026-04-28 23:09:08 +0300

    To be tested in class

--- JoshuaFolder/data_reading.py
+++ JoshuaFolder/data_reading.py
@@ -1,22 +1,29 @@
 import json
 
-a = open("data.json", "r")
-storage = json.load(a)
-                   #set json file as list for entry storage
+def load_data():
+    i = open("data.json", "r")
+    data = json.load(i)
+    i.close()
+    return data
 
-data = {
-    "patient" : "Hillary Clinton",
-    "hr" : 90,              
-    "bp" : 138,
-    "timestamp" : 1234556533342424
-    }                        #dummy data
+def save_data(data):
+    i = open("data.json", "w")
+    json.dump(data, i)
+    i.close()
 
+def add_entry(entry):
+    data = load_data()
+    data.append(entry)
+    save_data(data)
+    send_mqtt(entry)
+# add_entry({"patient":"A", "hr":80, "bp":120, "timestamp":1})
+# add_entry({"patient":"B", "hr":85, "bp":122, "timestamp":2})
+# add_entry({"patient":"C", "hr":90, "bp":125, "timestamp":3})
+# print(load_data())
 
-storage.append(data)         #append "duh"
+#FAKE MQTT TEST
 
-b = open("data.json", "w")
-json.dump(storage, b)
-b.close()                    #dump to json file
+# def send_mqtt(data):
+#     print("MQTT")
+#     print(data)
 
-
-print(storage)
